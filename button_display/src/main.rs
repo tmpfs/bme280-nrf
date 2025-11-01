6fef2dae98907e1e5639c2a5ded1e0dc72c8c74f
muji <muji@tmpfs.org>
2025-11-01 08:46:56 +0800

    Prepare tasks and enum.

--- button_display/src/main.rs
+++ button_display/src/main.rs
@@ -1,9 +1,12 @@
 #![no_std]
 #![no_main]
 
+use bme280::i2c::BME280;
 use core::fmt::Write;
 use embassy_executor::Spawner;
+use embassy_futures::join::join;
 use embassy_nrf::bind_interrupts;
+use embassy_nrf::gpio::{Input, Pull};
 use embassy_nrf::{
     gpio::{Level, Output, OutputDrive},
     peripherals,
@@ -13,12 +16,62 @@ use embassy_time::{Delay, Timer};
 use heapless::String;
 use libm::{fabsf, roundf, truncf};
 use max7219::MAX7219;
+use max7219::connectors::PinConnector;
 use static_cell::ConstStaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
     TWISPI0 => twim::InterruptHandler<peripherals::TWISPI0>;
 });
+
+#[repr(u8)]
+enum DisplayState {
+    Temp = 1,
+    Humidity = 2,
+    Pressure = 3,
+}
+
+async fn wait_for_pull_up(mut input: Input<'static>) -> ! {
+    loop {
+        input.wait_for_any_edge().await;
+        defmt::info!("is_low: {}, is_high = {}", input.is_low(), input.is_high());
+    }
+}
+
+async fn refresh_display(
+    mut bme: BME280<Twim<'static>>,
+    mut display: MAX7219<PinConnector<Output<'static>, Output<'static>, Output<'static>>>,
+) -> ! {
+    loop {
+        match bme.measure(&mut Delay) {
+            Ok(measurements) => {
+                defmt::info!("Relative Humidity = {}%", measurements.humidity);
+                defmt::info!("Temperature = {} deg C", measurements.temperature);
+                defmt::info!("Pressure = {} pascals", measurements.pressure);
+
+                let humidity = truncf(measurements.humidity) as i32;
+                let temp = measurements.temperature;
+                let int_temp = truncf(temp) as i32;
+                let int_temp = int_temp.min(85);
+                let int_temp = int_temp.max(-40);
+                let frac_temp = (roundf(fabsf(temp - truncf(temp)) * 10.0)) as u32;
+                let frac_temp = frac_temp.min(9);
+
+                let mut s = String::<8>::new();
+                write!(&mut s, "{:02}{}C{:03}H", int_temp, frac_temp, humidity).unwrap();
+                defmt::info!("{}", s.as_str());
+
+                let buf: [u8; 8] = s.as_bytes().try_into().unwrap();
+                // let dots = if int_temp < 0 { 0b00100000 } else { 0b01000000 };
+                display.write_str(0, &buf, 0b01000000).unwrap();
+            }
+            Err(_) => {
+                defmt::warn!("failed to measure BME280 sensor");
+            }
+        }
+        Timer::after_secs(5).await;
+    }
+}
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -55,33 +108,6 @@ async fn main(_spawner: Spawner) {
     let mut bme = bme280::i2c::BME280::new_primary(i2c);
     bme.init(&mut Delay).expect("to init bme280 sensor");
 
-    loop {
-        match bme.measure(&mut Delay) {
-            Ok(measurements) => {
-                defmt::info!("Relative Humidity = {}%", measurements.humidity);
-                defmt::info!("Temperature = {} deg C", measurements.temperature);
-                defmt::info!("Pressure = {} pascals", measurements.pressure);
-
-                let humidity = truncf(measurements.humidity) as i32;
-                let temp = measurements.temperature;
-                let int_temp = truncf(temp) as i32;
-                let int_temp = int_temp.min(85);
-                let int_temp = int_temp.max(-40);
-                let frac_temp = (roundf(fabsf(temp - truncf(temp)) * 10.0)) as u32;
-                let frac_temp = frac_temp.min(9);
-
-                let mut s = String::<8>::new();
-                write!(&mut s, "{:02}{}C{:03}H", int_temp, frac_temp, humidity).unwrap();
-                defmt::info!("{}", s.as_str());
-
-                let buf: [u8; 8] = s.as_bytes().try_into().unwrap();
-                // let dots = if int_temp < 0 { 0b00100000 } else { 0b01000000 };
-                driver.write_str(0, &buf, 0b01000000).unwrap();
-            }
-            Err(_) => {
-                defmt::warn!("failed to measure BME280 sensor");
-            }
-        }
-        Timer::after_secs(5).await;
-    }
+    let input = Input::new(p.P1_05, Pull::Up);
+    join(wait_for_pull_up(input), refresh_display(bme, driver)).await;
 }
